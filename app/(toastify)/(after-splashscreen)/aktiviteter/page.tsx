ecdf908e6262cb5d9bab0097c57dd623eadc70d6
Martin <martinbruun1995@gmail.com>
2024-10-02 12:06:16 +0200

    asdad

--- app/(toastify)/(after-splashscreen)/aktiviteter/page.tsx
+++ app/(toastify)/(after-splashscreen)/aktiviteter/page.tsx
@@ -7,7 +7,7 @@ async function Page() {
   const data = await getData('activities')
   return (
     <>
-      <HeaderTitle title="Aktiviteter" />
+      <HeaderTitle title="Aktivitet" />
       <div className="container pt-8">
         <section className="flex flex-col items-center gap-8 pb-24">
           {data.map((activity: Activity) => (
